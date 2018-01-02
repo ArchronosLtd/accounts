81fa613421bb2ebd1f0fc5696f868c788f809b69
DrogoNevets <steve.colemanwilliams@gmail.com>
2018-01-02 22:25:59 +0000

    error catching

--- src/services/mongo.js
+++ src/services/mongo.js
@@ -1,14 +1,24 @@
 const q = require('q'),
 	mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/accounts', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/accounts', {
+	useMongoClient: true
+});
 
-let Account = mongoose.model('Account', {
-	name: String,
-	startingAmount: Number,
-	amountPaid: Number,
-	amountPending: Number,
-	amountRemaining: Number
+let db = mongoose.connection,
+	Account;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+	console.log('mongo connected');
+
+	Account = mongoose.model('account', mongoose.Schema({
+		name: String,
+		startingAmount: Number,
+		amountPaid: Number,
+		amountPending: Number,
+		amountRemaining: Number
+	}));
 });
 
 module.exports = {
@@ -16,7 +26,7 @@ module.exports = {
 		let def = q.defer();
 
 		Account.find((err, accounts) => {
-			if(err) {
+			if (err) {
 				console.error(err);
 				def.reject(err);
 				return;

a0a661cad5bddeeebb65dd531f4e4013635ded53
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2018-01-03 09:58:16 +0000

    Add files via upload

--- src/services/transaction.js
+++ src/services/transaction.js
@@ -1,10 +1,21 @@
-const mongoSvc = require('./mongo');
+const q = require('q'),
+	mongoSvc = require('./mongo');
 
 module.exports = {
 	save: (transaction) => {
 		return mongoSvc.storeTransaction(transaction);
 	},
 	patch: (id, status) => {
-		mongoSvc.patchTransaction(id, status);
+		let def = q.defer();
+
+		mongoSvc.patchTransaction(id, status).then((transaction) => {
+			if (status === 'APPROVED') {
+				// update the account here
+			}
+
+			def.resolve(transaction);
+		});
+
+		return def.promise;
 	}
 }

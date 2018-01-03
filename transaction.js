bf50c4bd5466714c6917adc7293353bb13b1dae7
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2018-01-03 09:31:14 +0000

    not sure

--- transaction.js
+++ transaction.js
@@ -1,11 +1,7 @@
-const transactionSvc = require('../services/transaction');
+const mongoSvc = require('./mongo');
 
 module.exports = {
-	POST: (req, res) => {
-		transactionSvc.save(req.body).then((transaction) => {
-			req.send(transaction).status(200).end();
-		}).catch((err) => {
-			req.send(err).status(500).end();
-		});
+	save: (transaction) => {
+		return mongoSvc.storeTransaction(transaction);
 	}
 }

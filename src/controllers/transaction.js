97c9b89e2d9c46a8766c99237d25fb7dfe68bb54
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2018-01-03 09:37:33 +0000

    send the response not request

--- src/controllers/transaction.js
+++ src/controllers/transaction.js
@@ -3,9 +3,9 @@ const transactionSvc = require('../services/transaction');
 module.exports = {
 	POST: (req, res) => {
 		transactionSvc.save(req.body).then((transaction) => {
-			req.send(transaction).status(200).end();
+			res.send(transaction).status(200).end();
 		}).catch((err) => {
-			req.send(err).status(500).end();
+			res.send(err).status(500).end();
 		});
 	}
 }

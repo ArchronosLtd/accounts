82e643bf9ddaba1eda0fb78e13952d6801fc47e8
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2018-01-03 13:49:15 +0000

    update account

--- src/services/transaction.js
+++ src/services/transaction.js
@@ -3,7 +3,24 @@ const q = require('q'),
 
 module.exports = {
 	save: (transaction) => {
-		return mongoSvc.storeTransaction(transaction);
+		return mongoSvc.storeTransaction(transaction).then((transaction) => {
+			mongoSvc.getAccounts({
+				_id: transaction.account
+			}).then((account) => {
+				account.amountPending += transaction.amount;
+
+				account.save((err, account) => {
+					if (err) {
+						console.error(err);
+						def.reject(err);
+					}
+
+					def.resolve(transaction);
+				});
+			});
+
+			return transaction;
+		});
 	},
 	patch: (id, status) => {
 		let def = q.defer();
@@ -11,9 +28,25 @@ module.exports = {
 		mongoSvc.patchTransaction(id, status).then((transaction) => {
 			if (status === 'APPROVED') {
 				// update the account here
-			}
+				mongoSvc.getAccounts({
+					_id: transaction.account
+				}).then((account) => {
+					account.amountPaid += transaction.amount;
+					account.amountRemaining -= transaction.amount;
+					account.amountPending -= transaction.amount;
 
-			def.resolve(transaction);
+					account.save((err, account) => {
+						if (err) {
+							console.error(err);
+							def.reject(err);
+						}
+
+						def.resolve(transaction);
+					});
+				});
+			} else {
+				def.resolve(transaction);
+			}
 		});
 
 		return def.promise;

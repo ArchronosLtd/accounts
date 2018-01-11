7629420ecd4ce26f4a42494410e9db0ad2b28801
Steve Coleman-Williams <steve.colemanwilliams@gmail.com>
2018-01-11 10:13:03 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 'use strict';
 const express = require('express'),
-	bodyParser = require('body-parser');
+	bodyParser = require('body-parser'),
+	multer = require('multer');
 
 let app = express();
 
 app.use(bodyParser.json());
+
+app.use(multer({
+	dest: `./uploads/`,
+	rename: function(fieldname, filename) {
+		return filename;
+	},
+}));
 
 app.listen(8080, () => {
 	require('./src/controllers').init(app);
